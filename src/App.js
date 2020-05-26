bf870e569b8e15324f46e229ed93e2c7563ed368
STUNC <tunc0687@hotmail.com>
2020-05-26 18:19:23 +0300

    API kurulumu ve Kategorilerin API den getirilmesi

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react'
 import Navi from './Navi';
 import CategoryList from './CategoryList';
 import ProductList from './ProductList';
 import { Container, Row, Col } from 'reactstrap';
 
-function App() {
-  let productInfo = {title: "Product List"};
-  let categoryInfo = {title: "Category List"};
+export default class App extends Component {
+  state = {
+    currentCategory: ""
+  }
 
-  return (
-    <div>
-      <Container>
-        <Row>
-          <Navi />
-        </Row>
-        <Row>
-          <Col xs="3">
-            <CategoryList info={categoryInfo} />
-          </Col>
-          <Col xs="9">
-            <ProductList info={productInfo} />
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+  changeCategory = (category) => {
+    this.setState({ currentCategory: category.categoryName })
+  }
+
+  render() {
+    let productInfo = { title: "Product List", currentCategory: this.state.currentCategory };
+    let categoryInfo = { title: "Category List", currentCategory: this.state.currentCategory };
+
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Navi />
+          </Row>
+          <Row>
+            <Col xs="3">
+              <CategoryList changeCategory={this.changeCategory} info={categoryInfo} />
+            </Col>
+            <Col xs="9">
+              <ProductList info={productInfo} />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    )
+  }
 }
-
-export default App;
--- src/ProductList.js
+++ src/ProductList.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
+import {Table} from 'reactstrap'
 
 export default class ProductList extends Component {
+
     render() {
         return (
             <div>
-                <h4>{this.props.info.title}</h4>
+                <h4>{this.props.info.title} - {this.props.info.currentCategory}</h4>
+                <Table>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Username</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                    </tbody>
+                </Table>
             </div>
         )
     }
